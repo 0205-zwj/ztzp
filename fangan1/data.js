d674123d8bbb869280aba9d80bd0db22dc278486
0205-zwj <150521221+0205-zwj@users.noreply.github.com>
2026-09-17 18:57:34 +0800

    Add files via upload

---
+++ fangan1/data.js
@@ -0,0 +1,85 @@
+﻿// 构件数据总对象，网页读取所有构件信息
+window.partData = {
+    // ========== 构件ID：1 ==========
+    "1": {
+        partId: "1",                 // 构件唯一编号
+        workNo: "KMG-B2-B6GZ-07",    // 加工编号
+        projectName: "昆明长水国际机场项目", // 项目名称
+        section: "B2钢柱",           // 构件所属区段
+        partType: "十字变箱型钢柱",  // 构件类型
+        material: "Q420GJC",         // 材质牌号
+        spec: "800×600×35",          // 截面规格
+        weight: "7591kg",            // 构件重量
+        quantity: "1套",             // 数量
+        assembleDate: "2026-09-10",  // 组对日期
+        weldDate: "2026-09-12",      // 焊接日期
+        blastDate: "2026-09-15",     // 喷砂除锈日期
+        paintDate: "2026-09-16",     // 涂装油漆日期
+        qcPerson: "廖云、袁胜刚",     // 质检负责人
+        shipDate: "2026-09-20",      // 出厂发货日期
+        designer: "王建松",          // 设计人员
+        manager: "杨星",             // 主管负责人
+        glbUrl: "models/1.glb" // 3D模型文件路径（models文件夹内glb文件名）
+    }
+    // ========== 新增构件模板，复制上面整块，修改内容即可 ==========
+    ,"2": {
+      partId: "2",
+      workNo: "GL-1-2",
+      projectName: "昆明长水国际机场项目",
+      section: "梁柱",
+      partType: "H型",
+      material: "Q420热轧钢",
+      spec: "200*100*60",
+      weight: "550kg",
+      quantity: "20",
+      assembleDate: "2026.9.17",
+      weldDate: "2026.9.10",
+      blastDate: "2026.9.11",
+      paintDate: "2026.9.12",
+      qcPerson: "赵文杰",
+      shipDate: "2026.9.14",
+      designer: "赵文杰",
+      manager: "常建佳",
+      glbUrl: "models/2.glb"
+    }
+,"1-v1": {
+      partId: "1-v1",
+      workNo: "GL-1-3",
+      projectName: "昆明长水国际机场项目",
+      section: "梁柱",
+      partType: "H型",
+      material: "Q420热轧钢",
+      spec: "200*100*60",
+      weight: "550kg",
+      quantity: "20",
+      assembleDate: "2026.9.17",
+      weldDate: "2026.9.10",
+      blastDate: "2026.9.11",
+      paintDate: "2026.9.12",
+      qcPerson: "赵文杰",
+      shipDate: "2026.9.14",
+      designer: "赵文杰",
+      manager: "常建佳",
+      glbUrl: "models/1-v1.glb"
+    }
+,"2-v1": {
+      partId: "2-v1",
+      workNo: "GL-1-3",
+      projectName: "昆明长水国际机场项目",
+      section: "梁柱",
+      partType: "H型",
+      material: "Q420热轧钢",
+      spec: "200*100*60",
+      weight: "550kg",
+      quantity: "20",
+      assembleDate: "2026.9.17",
+      weldDate: "2026.9.10",
+      blastDate: "2026.9.11",
+      paintDate: "2026.9.12",
+      qcPerson: "赵文杰",
+      shipDate: "2026.9.14",
+      designer: "赵文杰",
+      manager: "常建佳",
+      glbUrl: "models/2-v1.glb"
+    }
+}
